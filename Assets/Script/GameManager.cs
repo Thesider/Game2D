b3098b317321051e42c5891c08b3d0b736725f5f
Thuy Trang <truongtrang05032004@gmail.com>
2025-10-25 11:15:12 +0700

    Display properties of player on UI ( Health, Armor, Weapon, Ammo, Score)

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
@@ -7,14 +8,67 @@ public class GameManager : MonoBehaviour
 
     public int score = 0;
     [SerializeField] TextMeshProUGUI scoreText;
-    public void addScore(int point)
-    {
-        score += point;
-        updateScore();
-    }
-    private void updateScore()
-    {
-        scoreText.text = score.ToString();
 
+    [Header("Player References")]
+    [Tooltip("Kéo game object Player vào đây")]
+    public PlayerStatus playerStatus;
+
+    [Tooltip("Kéo GameObject Player vào đây")]
+    public PlayerCombat playerCombat;
+
+    [Header("UI Elements")]
+    [Tooltip("Kéo Slider HealthBar vào đây")]
+    public Slider healthSlider;
+    [Tooltip("Kéo Slider ArmorBar vào đây")]
+    public Slider armorSlider;
+    [Tooltip("Kéo Text AmmoText vào đây")]
+    public TextMeshProUGUI ammoText;
+    [Tooltip("Kéo Image WeaponIcon vào đây")]
+    public Image weaponIcon;
+    [Tooltip("Kéo Text WeaponNameText vào đây")]
+    public TextMeshProUGUI weaponNameText;
+    void Start()
+    {
+       if(playerStatus != null)
+        {
+            healthSlider.maxValue = playerStatus.maxHealth;
+            healthSlider.value = playerStatus.maxHealth;
+            armorSlider.maxValue = playerStatus.maxArmor;
+            armorSlider.value = 0;
+        }
     }
+
+    private void Update()
+    {
+        if (playerStatus != null)
+        {
+            healthSlider.value = playerStatus.GetCurrentHealth();
+            armorSlider.value = playerStatus.GetCurrentArmor();
+
+            scoreText.text = "SCORE: " + playerStatus.GetScore().ToString("D6"); // "D6" để hiển thị 6 chữ số, ví dụ: 000500
+        }
+        if (playerCombat != null )
+        {
+            ammoText.text = "AMMO: " + playerCombat.GetCurrentAmmo();
+
+            WeaponData currentWeapon = playerCombat.GetCurrentWeapon();
+            if (currentWeapon != null)
+            {
+                // Nếu vũ khí có icon, hiển thị nó. Nếu không, ẩn Image đi.
+                if (currentWeapon.weaponIcon != null)
+                {
+                    weaponIcon.sprite = currentWeapon.weaponIcon;
+                    weaponIcon.enabled = true;
+                }
+                else
+                {
+                    weaponIcon.enabled = false;
+                }
+                // Hiển thị tên vũ khí
+                weaponNameText.text = currentWeapon.weaponName;
+            }
+        }
+    }
+    
+  
 }
