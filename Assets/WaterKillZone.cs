be926d6a14da3c5c06ebd0aad2ebc5700293c04a
Phamtoanbach1 <ptbphamtoanbach@gmail.com>
2025-11-14 10:33:01 +0700

    MODIFIED WATERKILLZONE -  VAI CAI NHAM NHI

--- Assets/WaterKillZone.cs
+++ Assets/WaterKillZone.cs
@@ -5,11 +5,11 @@ public class WaterKillZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Kiểm tra nếu đối tượng va chạm có tag là "Player"
+        
         if (collision.CompareTag("Player"))
         {   
        
-            // Lấy component PlayerStatus trên player
+        
             PlayerStatus playerStatus = collision.GetComponent<PlayerStatus>();
 
             if (playerStatus != null)
